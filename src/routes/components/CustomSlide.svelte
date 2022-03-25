3cbd9812f54105c16eff4a97008e5f2aa60c0b04
hellovlady <danillppiont106@gmail.com>
2022-03-26 03:42:46 +0900

    frontend part updated

---
+++ src/routes/components/CustomSlide.svelte
@@ -0,0 +1,66 @@
+<script context="module">
+	import { Row, Col, Styles, Icon } from 'sveltestrap';
+	import { onMount } from 'svelte';
+</script>
+
+<script lang="ts">
+	let hue = [50];
+
+	onMount(() => {
+		console.log('onMount');
+	});
+</script>
+
+<Styles />
+
+<Row cols={{ lg: 12, md: 12, sm: 12 }}>
+	<Col xs="1" style="text-align: right;">
+		{#if hue[0] === 50}
+			<Icon name="circle-fill" style="color: grey;" />
+		{:else}
+			<Icon name="check-circle-fill" style="color: green;" />
+		{/if}
+	</Col>
+	<Col xs="11">
+		<div class="range_box">
+			<input
+				type="range"
+				class="range"
+				bind:value={hue[0]}
+				min="0"
+				max="100"
+				style="background: linear-gradient(to right, {hue[0] === 50
+					? 'grey'
+					: 'red'} 0%, {hue[0] === 50 ? 'grey' : 'red'} {hue[0]}%, {hue[0] === 50
+					? 'grey'
+					: '#fff'} {hue[0]}%, {hue[0] === 50 ? 'grey' : '#fff'} 100%); border:1px solid {hue[0] ===
+				50
+					? 'grey'
+					: 'red'}"
+			/>
+		</div>
+	</Col>
+</Row>
+
+<style>
+	.range {
+		-webkit-appearance: none;
+		appearance: none;
+		padding: 0;
+		cursor: pointer;
+		width: 100%;
+		height: 7px;
+		border-radius: 10px;
+	}
+	.range::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		border: 2px solid red;
+		background: #fff;
+		height: 14px;
+		width: 14px;
+		border-radius: 50%;
+	}
+	.range_box {
+		padding-right: 20px;
+	}
+</style>
---
+++ src/routes/slider.svelte
@@ -0,0 +1,103 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	import { Row, Col, Styles, Container } from 'sveltestrap';
+	import CustomSlide from './components/CustomSlide.svelte';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<Styles />
+
+<svelte:head>
+	<title>Slider</title>
+</svelte:head>
+
+<Container fluid>
+	<Row>
+		<h1>Choose Your Communication Style</h1>
+		<p>
+			Adjust the sliders to help us to communicate to you in a manner that fits your communication
+			style.
+		</p>
+	</Row>
+	<Row>
+		<div class="title">
+			<p>I know best to make decision.</p>
+			<p>I look others to make decision.</p>
+		</div>
+	</Row>
+	<CustomSlide />
+	<Row>
+		<div class="title">
+			<p>Give me the details.</p>
+			<p>Give me the big picture.</p>
+		</div>
+	</Row>
+	<CustomSlide />
+	<Row>
+		<div class="title">
+			<p>I move away from what i don't want.</p>
+			<p>I gravitate toward what I do want.</p>
+		</div>
+	</Row>
+	<CustomSlide />
+	<Row>
+		<div class="title">
+			<p>Let's do it. Make it happen.</p>
+			<p>Study & think it through first.</p>
+		</div>
+	</Row>
+	<CustomSlide />
+	<Row>
+		<div class="title">
+			<p>Tell me choice & possibilities.</p>
+			<p>Show me steps & methods.</p>
+		</div>
+	</Row>
+	<CustomSlide />
+	<Row>
+		<div class="title">
+			<p>Tell me about it. Let me hear it</p>
+			<p>Show me. Let me see it</p>
+		</div>
+	</Row>
+	<CustomSlide />
+</Container>
+
+<style>
+	h1 {
+		text-align: left;
+	}
+	.title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: 20px;
+		font-size: 14px;
+		width: 95%;
+		margin-left: auto;
+		margin-right: 0;
+	}
+	.title p {
+		width: 120px;
+		padding: 0;
+		margin: 0;
+		text-align: left;
+		color: #000;
+		line-height: 15px;
+		font-weight: 700;
+	}
+	.title p:last-child {
+		text-align: right;
+	}
+</style>
